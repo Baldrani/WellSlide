05d629ab3c9bd27d92b578da3b7e89c7dd0f55f6
Mayon Maël <mael.mayon@free.fr>
2017-05-05 17:07:59 +0200

    Un peu mieux mais c'est pas encore ça

--- js/script.js
+++ js/script.js
@@ -77,125 +77,28 @@
 // var width = parseFloat($('#slideshow').css('width'));
 // $('#rail').css('right', width+'px');
 // //https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Transitions/Using_CSS_transitions?redirectlocale=en-US&redirectslug=CSS%2FTutorials%2FUsing_CSS_transitions#Detecting_the_completion_of_a_transition
-// // var fire = true;
-// // el = document.getElementById('rail');
-// // el.addEventListener("transitionend", function(){
-// //     fire = true;
-// // }, true);
-//
-// //Call des fichiers json (trop lourd)
-// // var json = $.getJSON('https://www.skrzypczyk.fr/slideshow.php',function(data){
-// //     data.forEach(function(el){
-// //         $('#rail').append('<img src="'+el.url+'" alt="'+el.desc+'">');
-// //     })
-// // });
-//
-// //For debug
-// $('#rail').append('<img src="img/1.jpg" data-index="0">')
-// $('#rail').append('<img src="img/2.jpg" data-index="1">')
-// $('#rail').append('<img src="img/3.jpg" data-index="2">')
-// $('#rail').append('<img src="img/1.jpg" data-index="3">')
-// $('#rail').append('<img src="img/2.jpg" data-index="4">')
-// $('#rail').append('<img src="img/3.jpg" data-index="5">')
-//
-// if($('img').isVisible()
-//
-// $('#rail').css('width', $('#rail img').length * width+'px');
-//
-// var play; //Global scope
-// function wellslide() {
-//    'use strict';
-//
-//
-//    var el = arguments[0].el; // Correspond à #rail //Necessaire pour introduire dans une fonction ? Pk ?
-//    //Autoplay
-//    if(arguments[0].autoPlay){
-//       autoPlay(arguments[0].el);
-//       $('button:nth-of-type(2)').text('Pause');
-//    }
-//    if(arguments[0].bullets){
-//       $(el).parent().append('<ul class="dots"></ul>');
-//       for(var i = 0; i < $('img').length; i++){
-//          $('.dots').append('<li><button type="button" data-role="none" role="button" data-index="'+i+'"></button>');
-//       }
-//    }
-//
-//    function moveToSlide(index){
-//       $('#rail').animate({
-//         right: index*width,
-//       }, 1000, function() {
-//           $('#rail').append($('img:first'));
-//           $(this).css('right',0);
-//       });
-//       //Move to Index * width (transition do the style)
-//       //possibility for last and first : check if width = 0 or width = max and if so do the same as in the actual version
-//       //Si on arrive a #rail.width = x*width alors revenir en arrière de 1 cran ?
-//    }
-//    $('.dots button').click(function(){
-//          moveToSlide($(this).data('index'));
-//    });
-//
-//    //Slide Right
-//    $('#previous').click(function(){
-//        $(el).slideRight();
-//    })
-//    //Slide Left
-//    $('#next').click(function(){
-//        $(el).slideLeft();
-//    })
-//
-//    //Pause, play
-//    $('#action').click(function(){
-//       if(play){
-//          $('button:nth-of-type(2)').text('Play');
-//          clearInterval(play);
-//          play = null;
-//       } else {
-//          $('button:nth-of-type(2)').text('Pause');
-//          play = autoPlay(el);
-//       }
-//    })
-//    return this;
-// };
-//
-// //Wut :/ ?
-// function autoPlay(slide){
-//    clearInterval(play);
-//    play = window.setInterval(function(){
-//       $(slide).slideLeft();
-//    }, 2000);
-// }
-//
-// //Slide to slide directement ?
-// //Améliorer slide left pour pouvoir
-// $.fn.extend({
-//     slideLeft: function(){
-//         this.animate({
-//           right: "+="+width,
-//         }, 1000, function() {
-//             $('#rail').append($('img:first'));
-//             $(this).css('right',parseFloat($('#rail').css('right'))-width);
-//         });
-//     },
-//     slideRight: function(){
-//         this.animate({
-//           right: "-="+width,
-//         }, 1000, function() {
-//             $('#rail').prepend($('img:last'));
-//             $(this).css('right',parseFloat($('#rail').css('right'))+width);
-//         });
-//     }
-// });
+// var fire = true;
+// el = document.getElementById('rail');
+// el.addEventListener("transitionend", function(){
+//     fire = true;
+// }, true);
 
+//Call des fichiers json (trop lourd)
+// var json = $.getJSON('https://www.skrzypczyk.fr/slideshow.php',function(data){
+//     data.forEach(function(el){
+//         $('#rail').append('<img src="'+el.url+'" alt="'+el.desc+'">');
+//     })
+// });
 
 //Nouvelle version crade
 
 //TODO Utiliser promise() ?
 //TODO Play Pause, Hover
+//TODO SlideToSlide (On click on left or right, put slideToSlide to the prev or next and launch slideToSlide function())
 var width = parseFloat($('#slideshow').css('width'));
 $('#rail').css('right', '0px');
 //For debug
-$('#rail').append('<img src="img/1.jpg" data-index="0">')
+$('#rail').append('<img src="img/1.jpg" data-index="0" class="slideToSlide">')
 $('#rail').append('<img src="img/2.jpg" data-index="1">')
 $('#rail').append('<img src="img/3.jpg" data-index="2">')
 $('#rail').append('<img src="img/1.jpg" data-index="3">')
@@ -218,7 +121,7 @@ $('#rail').css('right',width);
 
 function wellslide(el){
     'use strict';
-    var _, play;
+    var _, play = false;
 
     //TODO Try to make the thing identic as
     function moveBullet(){
@@ -253,39 +156,21 @@ function wellslide(el){
         });
     }
 
-    $('#previous').click(function(e){
-        moveLeft();
-    });
+    // //On Hover
+    //TODO put in hold and if labs de temps au survole, relancer
+    // $('#slideshow').hover(function(){
+    //     $('#action').text('Play');
+    //     clearInterval(play);
+    //     play = false;
+    // },function(){
+    //     $('#action').text('Pause');
+    //     clearInterval(play);
+    //     play = setInterval(moveRight,2000);
+    // });
 
-    $('#next').click(function(){
-        moveRight();
-    });
-
-    //A initialiser avec les conditions
-    $('#action').click(function(){
-        if(play != false){
-            $(this).text('Play');
-            clearInterval(play);
-            play = false;
-        } else {
-            $(this).text('Pause');
-            clearInterval(play);
-            play = setInterval(moveRight,2000);
-        }
-    });
-
-    //On Hover
-    $('#slideshow').hover(function(){
-        $('#action').text('Play');
-        clearInterval(play);
-        play = false;
-    },function(){
-        $('#action').text('Pause');
-        clearInterval(play);
-        play = setInterval(moveRight,2000);
-    });
-
-    el.parent().append('<ul class="dots"></ul>');
+    /* Partie Dots */
+    //TODO : LOOL
+    el.parent().parent().parent().append('<ul class="dots"></ul>');
     for(var i = 0; i < $('img:not(".ghost")').length; i++){
         $('.dots').append('<li><button type="button" data-role="none" role="button" data-index="'+i+'"></button>');
     }
@@ -301,5 +186,23 @@ function wellslide(el){
       }, 1000);
     })
 
+    $('#previous').click(function(e){
+        moveLeft();
+    });
+
+    $('#next').click(function(){
+        moveRight();
+    });
+
+    $('#play').click(function(){
+        $(this).find('.fa').toggleClass('fa-pause-circle-o fa-play-circle-o');
+        console.log(false);
+        if(play == false){
+            play = window.setInterval(moveRight,2000);
+        } else {
+            clearInterval(play);
+            play = false;
+        }
+    })
 
 }
